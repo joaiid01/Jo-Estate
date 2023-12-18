389aaa33175cca1326c57fab07184c8da56e1423
Johnny <johnnyyo9404@gmail.com>
2023-12-18 23:23:17 +0300

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken"
 
 
 export const signUp= async (req,res,next)=>{
@@ -20,3 +20,20 @@ export const signUp= async (req,res,next)=>{
     }
 }
 
+export const signin= async (req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+     const validUser= await User.findOne({email:email})
+        if(!validUser)return next(errorHandler(404,"user not found"))
+      const validPassword=bcryptjs.compareSync(password,validUser.password)
+          if(!validPassword)return next(errorHandler(401,"invalid ceredintial"))
+         
+  const token=jwt.sign({id:validUser._id},process.env.JWT)
+  const {password:pass, ...rest}=validUser._doc;
+  res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
+      }
+      catch(error){
+  
+        next(errorHandler(550,"error from handler"));
+      }
+  }
